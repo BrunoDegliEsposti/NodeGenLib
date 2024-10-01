aee8ba79c2c6cf56c51f9f63e9070e0e2627bcf4
Bruno Degli Esposti <brunodegliesposti@gmail.com>
2024-10-01 22:25:54 +0200

    Release the library under the LGPL license

--- src/advancing_front.hpp
+++ src/advancing_front.hpp
@@ -1,3 +1,23 @@
+/*
+ * NodeGenLib - A library for advancing front node generation
+ * Copyright (C) 2024 Bruno Degli Esposti
+ *
+ * This file is part of NodeGenLib.
+ *
+ * NodeGenLib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * NodeGenLib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with NodeGenLib. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 #include <cstdio>
 #include <cstdlib>
